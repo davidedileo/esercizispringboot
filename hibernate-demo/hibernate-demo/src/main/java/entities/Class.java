7a0ff03b32ac09f76a5aeb522d1dbde7a6706d9a
davidedileo <160485277+davidedileo@users.noreply.github.com>
2024-05-16 15:38:16 +0200

    Add files via upload

---
+++ hibernate-demo/hibernate-demo/src/main/java/entities/Class.java
@@ -0,0 +1,24 @@
+package entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "classes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Class {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+}
