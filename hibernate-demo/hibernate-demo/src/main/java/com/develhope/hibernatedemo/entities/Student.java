af979bdeaf19bdbcbdbc85bda362e4a82721e09b
davidedileo <160485277+davidedileo@users.noreply.github.com>
2024-05-16 16:59:09 +0200

    Add files via upload

---
+++ hibernate-demo/hibernate-demo/src/main/java/com/develhope/hibernatedemo/entities/Student.java
@@ -0,0 +1,27 @@
+package com.develhope.hibernatedemo.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "students")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String firstNme;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+}
